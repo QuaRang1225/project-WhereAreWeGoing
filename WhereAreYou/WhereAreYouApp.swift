22af3f38060c7b2c7c2a2430dc53d24b05090284
QuaRang1225 <dbduddnd1225@gmail.com>
2023-07-01 14:24:15 +0900

    프로젝트 개설

---
+++ WhereAreYou/WhereAreYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhereAreYouApp.swift
+//  WhereAreYou
+//
+//  Created by 유영웅 on 2023/07/01.
+//
+
+import SwiftUI
+
+@main
+struct WhereAreYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
